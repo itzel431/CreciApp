3e09f862004b8e5ab93f8b27884f12aba1bbf4aa
Itzel <ie5123353@gmail.com>
2024-12-04 00:43:45 -0600

    kk

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import 'react-native-gesture-handler';  // Importa esto primero
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text, Button } from 'react-native';
+
+// Pantallas de tu aplicación
 import HomeScreen from './screens/HomeScreen';
 import GrowthScreen from './screens/GrowthScreen';
 
@@ -11,15 +14,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="Growth" 
-          component={GrowthScreen} 
-        />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Growth" component={GrowthScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
